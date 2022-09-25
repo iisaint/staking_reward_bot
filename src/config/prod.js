14e645e90391fcc729471230604fbd307d5a25df
yaohsin <iisaintz@gmail.com>
2022-09-25 11:53:24 +0800

    handle env value type

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
   SUBSCAN_API_KEY: process.env.SUBSCAN_API_KEY,
-  SUBSCAN_API_DELAY: process.env.SUBSCAN_API_DELAY,
-  DEFAULT_STASH: process.env.DEFAULT_STASH,
+  SUBSCAN_API_DELAY: Number(process.env.SUBSCAN_API_DELAY),
+  DEFAULT_STASH: JSON.parse(process.env.DEFAULT_STASH),
 }
